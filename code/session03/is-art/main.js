9eac936d01c46bfe9eddbbdd9fd4046910556db7
Doug Rosman <drosman@saic.edu>
2021-09-28 20:58:33 -0500

    fix main.js

--- code/session03/is-art/main.js
+++ code/session03/is-art/main.js
@@ -2,8 +2,17 @@ ethereum.autoRefreshOnNetworkChange = false;
 ethereum.enable();
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-let signer = provider.getSigner(); // the person signed into mask is the 'signer'
-const contractAddress = "0x89ec369cBdD2B16a136cd0d6f4Cc33b984f184b9"; // insert your contract address here
+let signer = provider.getSigner(); // the person signed into metamask is the 'signer'
+
+
+// \/ \/ \/ \/ CHANGE THIS EACH TIME YOU DEPLOY A NEW CONTRACT \/ \/ \/ \/
+
+// Insert your contract address here.
+const contractAddress = "0x89ec369cBdD2B16a136cd0d6f4Cc33b984f184b9"; 
+
+// /\ /\ /\ /\ CHANGE THIS EACH TIME YOU DEPLOY A NEW CONTRACT /\ /\ /\ /\
+
+
 const contractABI = [
   "function changeStatus() public",
   "function viewStatus() public view returns (string memory)"
@@ -12,10 +21,15 @@ const contractABI = [
 // we use the 'contract' variable when we need to read from the blockchain
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
+
 // we use the 'tokenWithSigner' variable when we need to do a transaction that
 // involves spending ETH (e.g. sending ETH, sending tokens, writing to a contract)
 const tokenWithSigner = contract.connect(signer);
 
+
+// we use the 'tokenWithSigner' variable when we need to do a transaction that
+// involves spending ETH (e.g. sending ETH, sending tokens, writing to a contract)
+const tokenWithSigner = contract.connect(signer);
 
 // executes the status change upon clicking the button
 $('.change-status').click(changeStatus)
@@ -25,20 +39,24 @@ viewStatus();
 
 // an asynchrononous function to view the art status of the contract
 async function viewStatus() {
-    let status = await contract.viewStatus();
+  
+  // view the art status of the contract on the blockchain
+  let status = await contract.viewStatus();
 
-    $('.is-art').text(`${status}`);
-    // console.log(status);
+  // update the HTML to display the status
+  $('.is-art').text(`${status}`);
 }
 
 // Change the status of the contract between "is not art" and "is art"
+
+// You will need to pay a small gas fee to change the status.
 function changeStatus() {
     tokenWithSigner.changeStatus();
 }
 
-// a not so elegant but totally functioning way to automatically
+// a not so elegant but totally functional way to automatically
 // update the displayed status of the contract after changing it
+// every 2 seconds, check the blockchain to see if the status has changed
 setInterval(function(){
     viewStatus();
-    // console.log("checking...")
 }, 2000);
