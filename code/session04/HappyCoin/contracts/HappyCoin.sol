3550424f9930a265cb968a2ce38b8d92efba1f55
Doug Rosman <drosman@saic.edu>
2021-09-28 22:16:51 -0500

    make main.js more user-friendly

--- code/session04/HappyCoin/contracts/HappyCoin.sol
+++ code/session04/HappyCoin/contracts/HappyCoin.sol
@@ -29,39 +29,55 @@
 pragma solidity ^0.8.0;
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
+// change 'HappyCoin' to the name of your contract
 contract HappyCoin is ERC20 {
     
     string private myTokenName = 'Happy Coin'; // 1. The name of your of your Token
     string private myTokenSymbol = 'HAP'; // 2. A 3-letter symbol, like a stock symbol
-    uint256 private myAmount = 2000; // 3. The amount of tokens you want to create*
-    uint8 private myNumDecimals = 0; // 4. The number of decimal places, 0-18 (The ERC20 default is 18)
-    // bool private isFixedSupply = false; // 5. set to 'true' if you don't want to mint more of this token in the future
-    
-    
-    // address public admin;
+    uint256 private myAmount = 1000; // 3. The amount of tokens you want to create*
+    uint8 private myNumDecimals = 18; // 4. The number of decimal places, 0-18 (The ERC20 default is 18)
+    bool private isFixedSupply = false; // 5. set to 'true' if you don't want to mint more of this token in the future
+    address public admin;
     
     constructor() ERC20(myTokenName, myTokenSymbol) {
-        _mint(msg.sender, myAmount * 10 ** decimals());
-        _transfer(msg.sender, address(this), 1000);
-        // admin = msg.sender;
+
+        uint256 totalSupply = myAmount * 10 ** decimals();
+
+
+        // how many tokens you want to send to this contract
+        uint256 amountForContract = totalSupply/2;
+        // (it must be less than or equal to the amount you mint for yourself (amountForMinter))
+        // by default, totalSupply/2 means that half of the tokens you mint will be sent to the contract
         
+
+        admin = msg.sender;
+        _mint(msg.sender, totalSupply);
+        
+        // ensures that you don't send more tokens to the contract than are available in your wallet
+        require(amountForContract <= totalSupply);
+        _transfer(msg.sender, address(this), amountForContract);
     }
     
     function decimals() public view override returns (uint8) {
         return myNumDecimals;
     }
     
-    function reward(uint amt) external {
+    // this function rewards participants by sending tokens directly from this contract
+    // this is the function we call in our main.js file on our website
+    function reward(uint256 amt) external {
         uint256 contractBalance = balanceOf(address(this));
         require(amt > 0, "No reward??");
         require(amt <= contractBalance, "Not enough tokens in the reserve");
+        require(!isFixedSupply, 'fixed supply, cannot mint more');
+        require(msg.sender == admin, 'only admin can mint');
         _transfer(address(this), msg.sender, amt);
-        // require(!isFixedSupply, 'fixed supply, cannot mint more');
-        // require(msg.sender == admin, 'only admin can mint');
     }
-    
-    function burn(uint amount) external {
-        _burn(msg.sender, amount);
+
+    // this function allows the original creator of this contract to
+    // mint more tokens and increase the supply
+    function mint(address to, uint256 amount) external {
+        require(!isFixedSupply, 'fixed supply, cannot mint more');
+        require(msg.sender == admin, 'only admin can mint');
+        _mint(to, amount);
     }
-    
 }
