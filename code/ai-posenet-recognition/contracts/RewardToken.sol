9b5a31b08f039f9a0f148be4a43d0f4fb8f45560
Doug Rosman <drosman@saic.edu>
2021-11-30 22:09:20 -0600

    add posenet recognition

---
+++ code/ai-posenet-recognition/contracts/RewardToken.sol
@@ -0,0 +1,87 @@
+/*
+  SPDX-License-Identifier: UNLICENSED
+  Contract created by Douglas Rosman for Blockchain: The New Internet
+  Code assistance from this YouTube Tutorial:
+  Create ERC20 token on Ethereum (the EASY way) - EatTheBlocks
+  https://www.youtube.com/watch?v=ZLFiGHIxS1c
+  
+  //////////// DESCRIPTION ////////////
+  This is contract allows you to deploy your own cryptocurrency on the
+  Ethereum Blockchain in the form of an ERC20 token. This contract extends
+  the ERC20 template contract from OpenZeppelin –
+  (https://github.com/OpenZeppelin/openzeppelin-contracts)
+  
+  More information about ERC20 tokens:
+  https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
+  
+  //////////// INSTRUCTIONS ////////////
+  Before deploying your contract...
+  1. Change the file name from 'MyToken.sol' to 'WhateverYourTokenNameIs.sol'
+  2. Change the name of your contract to match your file name.
+  3. Change the following parameters of your token:
+    3-1. Token name
+    3-2. Token symbol
+    3-3. Token amount
+    3-4. Number of decimals
+    3-5. Whether or not your token has a fixed supply
+*/
+
+pragma solidity ^0.8.0;
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+// change 'HappyCoin' to the name of your contract
+contract RewardToken is ERC20 {
+    
+    string private myTokenName = 'WeArToken'; // 1. The name of your of your Token
+    string private myTokenSymbol = 'WAT'; // 2. A 3-letter symbol, like a stock symbol
+    uint256 private tokensForMe = 30; // 3. The amount of tokens you want to create for yourself
+    uint256 private tokensForContract = 2000; // 4. The amount of tokens you want to give to the contract
+    uint8 private myNumDecimals = 0; // 5. The number of decimal places, 0-18 (The ERC20 default is 18)
+    bool private isFixedSupply = false; // 6. set to 'true' if you don't want to mint more of this token in the future
+    address public admin;
+    
+
+    constructor() ERC20(myTokenName, myTokenSymbol) {
+        
+        // set the admin to the deployer of the contract
+        admin = msg.sender;
+
+        // mint tokens to the owner of the contract (you)
+        _mint(msg.sender, tokensForMe * 10 ** decimals());
+
+        // mint tokens directly to the contract
+        _mint(address(this), tokensForContract * 10 ** decimals());
+    }
+    
+    function decimals() public view override returns (uint8) {
+        return myNumDecimals;
+    }
+    
+    // this function rewards participants by sending tokens directly from this contract
+    function reward(uint256 amt) external {
+        uint256 contractBalance = balanceOf(address(this));
+        require(amt > 0, "No reward??");
+        require(amt <= contractBalance, "Not enough tokens in the reserve");
+        require(!isFixedSupply, 'fixed supply, cannot mint more');
+        // require(msg.sender == admin, 'only admin can mint');
+        _transfer(address(this), msg.sender, amt);
+    }
+
+    // this function allows the original creator of this contract to
+    // mint more tokens and increase the supply
+    function mint(address to, uint256 amount) external {
+        require(!isFixedSupply, 'fixed supply, cannot mint more');
+        require(msg.sender == admin, 'only admin can mint');
+        _mint(to, amount);
+    }
+
+    // allows the admin of the contract to be changed by the current admin
+    function changeAdmin(address newAdmin) public {
+        require (msg.sender == admin, 'only admin can change the admin');
+        admin = newAdmin;
+    }
+    
+    function viewAdmin() public view returns (address) {
+        return admin;
+    }
+}
---
+++ code/ai-posenet-recognition/reward-interaction.js
@@ -0,0 +1,175 @@
+////////////////////////////////////////
+//           ETHEREUM SETUP           //
+////////////////////////////////////////
+
+ethereum.autoRefreshOnNetworkChange = false;
+window.ethereum.enable();
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+// swap this address out with the contract address for your token (found in Remix after deploying)
+const contractAddress = "0x290a027cC8A22746d020AB07CC8d7fa2aA194c9d";
+const contractABI = [
+  "function name() public view returns (string memory)",
+  "function symbol() public view returns (string memory)",
+  "function decimals() public view returns (uint8)",
+  "function totalSupply() public view returns (uint256)",
+  "function balanceOf(address account) public view returns (uint256)",
+  "function transfer(address recipient, uint256 amount) public returns (bool)",
+  "function allowance(address owner, address spender) public view returns (uint256)",
+  "function approve(address spender, uint256 amount) public returns (bool)",
+  "function transferFrom(address sender, address recipient, uint256 amount) public returns (bool)",
+  "function increaseAllowance(address spender, uint256 addedValue) public returns (bool)",
+  "function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool)",
+  "function reward(uint256 amt) public payable",
+  "function mint(address to, uint256 amount) external"
+];
+
+
+const contract = new ethers.Contract(contractAddress, contractABI, provider);
+const tokenWithSigner = contract.connect(signer);
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+////////////////////////////////////////
+//           POSENET SETUP            //
+////////////////////////////////////////
+
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "./model/my-pose-model/";
+let model, webcam, ctx, labelContainer, maxPredictions;
+
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // Note: the pose library adds a tmPose object to your window (window.tmPose)
+  model = await tmPose.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  // Convenience function to setup a webcam
+
+  const width = 320;
+  const height = 180;
+  const flip = true; // whether to flip the webcam
+  webcam = new tmPose.Webcam(width, height, flip); // width, height, flip
+  await webcam.setup(); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  // append/get elements to the DOM
+  const canvas = document.getElementById("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  ctx = canvas.getContext("2d");
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) { // and class labels
+    labelContainer.appendChild(document.createElement("div"));
+  }
+}
+
+async function loop(timestamp) {
+  webcam.update(); // update the webcam frame
+  
+  await predict();
+  
+  window.requestAnimationFrame(loop);
+}
+
+
+let winningCounter = 0;
+let winningLabel = "";
+let shouldMint = true;
+
+
+
+async function predict() {
+  // Prediction #1: run input through posenet
+  // estimatePose can take in an image, video or canvas html element
+  const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+  // Prediction 2: run input through teachable machine classification model
+
+  let winningProbability = 0;
+  let currentLabel = "";
+  let nextWinningLabel = "";
+  const prediction = await model.predict(posenetOutput);
+  
+  for (let i = 0; i < maxPredictions; i++) {
+    
+    // store current class name and probability
+    currentLabel = prediction[i].className;
+    const currentProbability = prediction[i].probability.toFixed(4);
+    
+    // finds the highest probability among the different classes
+    if (currentProbability > winningProbability) {
+      winningProbability = currentProbability;
+      nextWinningLabel = currentLabel;
+    }
+    //const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    // labelContainer.childNodes[i].innerHTML = classPrediction;
+  }
+
+  // keeps track of how long the winning label has been predicted
+  if(winningLabel == nextWinningLabel) {
+    winningCounter++;
+  } else {
+    winningCounter = 0;
+    winningLabel = nextWinningLabel;
+  }
+  $('#pose-meter').css('background', `rgb(0, ${winningCounter*5}, 0)`).css('transform', `scaleX(${winningCounter*4})`);
+
+
+  console.log("winningLabel: ", winningLabel);
+  console.log("winningCounter: ", winningCounter);
+
+
+  // SEND TRANSACTIONS DEPENDING ON WHICH POSE IS HELD THE LONGEST
+
+  const HOLD_THRESHOLD = 20; // how many times in a row a pose must be predicted in order to call it the winner
+
+  let REWARD_AMOUNT = 10; // how many tokens you want to reward
+  
+  // REWARD_AMOUNT = BigInt(REWARD_AMOUNT * Math.pow(0, decimals));
+  
+  if(winningCounter > HOLD_THRESHOLD && shouldMint) {
+    
+    if(winningLabel == "pose 1") {
+      console.log("pose 1 is the winner");
+      tokenWithSigner.reward(REWARD_AMOUNT);
+    } else if (winningLabel == "pose 2") {
+      console.log("pose 2 is the winner");
+      tokenWithSigner.reward(REWARD_AMOUNT * 2);
+    } else if (winningLabel == "pose 3") {
+      console.log("pose 3 is the winner");
+      tokenWithSigner.reward(REWARD_AMOUNT * 3);
+    } else {
+      console.log("resting is the winner");
+    }
+
+    shouldMint = false;
+  }
+
+  // finally draw the poses (comment this out if you don't want to draw the skeleton)
+  drawPose(pose);
+}
+
+function drawPose(pose) {
+  if (webcam.canvas) {
+    ctx.drawImage(webcam.canvas, 0, 0);
+    // draw the keypoints and skeleton
+    if (pose) {
+      const minPartConfidence = 0.5;
+      tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+      tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+    }
+  }
+}
