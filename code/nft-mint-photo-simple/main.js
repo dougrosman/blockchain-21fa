d28d6065d9760512883fa900a9f49fac73c1a56b
Doug Rosman <drosman@saic.edu>
2021-12-09 17:18:34 -0600

    add minting function

--- code/nft-mint-photo-simple/main.js
+++ code/nft-mint-photo-simple/main.js
@@ -5,13 +5,13 @@ const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
 // swap this address out with the contract address for your token (found in Remix after deploying)
-const contractAddress = "0xd3aa4347580354A6b69E820C7dECF33af018DAe6"; // swap out with your address
+const contractAddress = "0x9aee9F65A89abde08767540c19588d6D963fc353"; // swap out with your address
 const contractABI = [
     "function balanceOf(address account) public view returns (uint256)",
     "function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256)",
     "function totalSupply() public view returns (uint256)",
     "function tokenByIndex(uint256 index) public view returns (uint256)",
-    "function mintPhoto(address from, address to, uint256 tokenId) public",
+    "function mintPhoto(string tokenURI) public",
     "function tokenURI(uint256 tokenId) public view returns (string)"
   ];
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
@@ -22,7 +22,19 @@ const tokenWithSigner = contract.connect(signer);
 
 ////// END ETHEREUM SETUP
 
-$('#reward-button').click(function(){
+$('.gallery img').click(function() {
 
-  tokenWithSigner.mint(numTokens);
+  const imgFilePath = $(this)[0].src
+  const imgFilePathSplit = imgFilePath.split("/");
+  const imgFileName = imgFilePathSplit[imgFilePathSplit.length-1];
+
+  console.log(imgFilePath);
+  console.log(imgFileName);
+
+  tokenWithSigner.mintPhoto(imgFilePath);
 })
+
+// $('#reward-button').click(function(){
+
+//   tokenWithSigner.mint(numTokens);
+// })
