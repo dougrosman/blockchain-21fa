e1788d397196374324ed01b164dcdcf74df41061
Doug Rosman <drosman@saic.edu>
2021-12-09 23:09:45 -0600

    add alerts

--- code/nft-mint-photo-simple/main.js
+++ code/nft-mint-photo-simple/main.js
@@ -30,25 +30,33 @@ async function main() {
   // when the user clicks on an image in the gallery...
   $('.gallery img').click(function() {
 
-    // extract the filename of the image that was clicked
+    // extract the filepath of the image that was clicked (img src)
+    // e.g. https://yourusername.github.io/photo-project/img/1.jpg
     const imgFilePath = $(this)[0].src;
+
+    // split the filepath based on "/"
     const imgFilePathSplit = imgFilePath.split("/");
+
+    // extract the filename from the filepath
+    // e.g. "1.jpg"
     const imgFileName = imgFilePathSplit[imgFilePathSplit.length-1];
 
+    // display the filename of the image to be minted
     $('.info').text(`File to mint: ${imgFileName}`)
-    console.log(imgFilePath);
-    console.log(imgFileName);
 
+
+    // show alert messages (or don't...)
     errorMessage = true; // SET THIS TO 'false' TO BLOCK THE ALERT MESSAGES
     if(errorMessage == true) {
       alerts();
     }
     
+    // initiate the transaction to mint an NFT that passes the URL of the
+    // photo that was clicked as the tokenURI to the NFT, so that the NFT
+    // points to the image file.
     tokenWithSigner.mintPhoto(imgFilePath);
   })
 }
-
-
 
 // These alerts will appear if you try to mint an NFT while testing your code in Live Server
 function alerts() {
@@ -57,6 +65,8 @@ function alerts() {
     
     alert(`If you approve the MetaMask transaction that appears after you close this dialog box, the link to the image in the NFT you mint will look a bit like this: "http://127.0.0.1:5500/img/1.jpg" ... this link only works locally, and will not be publicly viewable.`)
 
-    alert(`Feel free to block these alerts so they don't appear every time you try to test your code. Just be aware that MINTING NFTs WHILE USING LIVE SERVER will result in NFTs with inaccessible images. (You can block these alerts in the main.js file)`)
+    alert(`Feel free to block these alerts so they don't appear every time you try to test your code. Just be aware that MINTING NFTs WHILE USING LIVE SERVER will result in NFTs with inaccessible images.`)
+
+    alert(`(You can block these alerts in the main.js file)`)
   }
 }
